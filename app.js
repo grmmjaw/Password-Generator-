7d44dc59d1b2cd9ea2a1f229bfdb6a4b28b79344
Mj <mjomamogho@gmail.com>
2025-12-28 22:14:40 +0000

    add loop to select from 88 characters

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+
+
 function generatePassword(){
-    console.log("password was generated")
+    for(let i = 0; i < 88; i++){
+   let input = Math.floor(Math.random()*88)
+   let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?"
+   characters.charAt(input)
+   console.log(characters.charAt(input))}
 };
+
+
+
 function deletePassword(){
     console.log("password deleted")
 };
