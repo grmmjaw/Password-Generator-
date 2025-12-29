5fa7d0bd3be2f5e197e555e707eaea8ffdf57688
Mj <mjomamogho@gmail.com>
2025-12-29 01:27:54 +0000

    add functional copy button

--- app.js
+++ app.js
@@ -12,14 +12,31 @@ text += randomCharacters}
     display.value = text
 };
 
+function copyPassword(){
+      const copyBtn = document.getElementById("copyBtn")
+    if (!display.value 
+){alert("Error Password Not Generated")}
 
+else {const cb = navigator.clipboard;
+    cb.writeText(display.value);
+copyBtn.textContent ="Copied :]"
+}
+setTimeout(() => {
+    copyBtn.textContent = "Copy"
+}, 1500
 
+)
+
+}
 
 function deletePassword(){
     display.value = ""
 };
 const generateBtn = document.getElementById("generateBtn");
 const deleteBtn = document.getElementById("deleteBtn");
+const copyBtn = document.getElementById("copyBtn");
 const display = document.getElementById("display");
 generateBtn.addEventListener("click",generatePassword);
 deleteBtn.addEventListener("click",deletePassword);
+
+
